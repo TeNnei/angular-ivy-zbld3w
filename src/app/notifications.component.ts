847d41109561acae9ade4e751a568cd23cc3cad1
TeNnei <70272751+TeNnei@users.noreply.github.com>
2022-12-03 15:41:59 +0500

    Initial commit

---
+++ src/app/notifications.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'notifcaitons-component',
+  templateUrl: './notifications.components.html',
+  styleUrls: ['./notifications.components.css']
+})
+export class Notificaitons{
+
+}
