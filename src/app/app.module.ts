b25bb8035ff276892695fd1453f3b8438bf5aa24
TeNnei <70272751+TeNnei@users.noreply.github.com>
2022-12-06 16:35:31 +0500

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { AppComponent } from './app.component'
+
 import { Notificaitons } from './notifications.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ Notificaitons],
+  declarations: [
+    AppComponent,
+    Notificaitons
+    ],
   bootstrap:    [ Notificaitons ]
 })
 export class AppModule { }
